5bc1ea446a07c485cbc05aab0323b6de0e24be54
ews0404 <eric.stiers@gmail.com>
2025-07-29 20:55:46 +1200

    made all input values f32, added validation against negative dimension/weight entries

--- src/main.rs
+++ src/main.rs
@@ -4,46 +4,59 @@ use std::iter::zip;
 // settings
 const MAX_WEIGHT_KG:f32 = 25.0;
 
+// used to ensure we get positive length/weight only
+fn f32_is_positive(s: &str) -> Result<f32, String> {
+    let x: f32 = s.parse().map_err(|_| format!("'{}' is not a valid float", s))?; // this is a bit of copy-paste 
+    if x >= 0.0 { 
+         Ok(x)
+    } else {
+        Err(format!("value {} cannot be negative!", x))
+    }
+}
 
 #[derive(Parser, Debug)]
 #[command(version, about="\n\ndefault units are (mm, kg)", long_about = None)]
 struct Cli{
     /// height of object
-    x:u32,
+    #[clap(value_parser=f32_is_positive)]
+    x:f32,
 
     /// width of object
-    y:u32,
+    #[clap(value_parser=f32_is_positive)]
+    y:f32,
 
     /// depth of object 
-    z:u32,
+    #[clap(value_parser=f32_is_positive)]
+    z:f32,
 
     /// weight of object
+    #[clap(value_parser=f32_is_positive)]
     w:f32,
 
     /// optional coversion factor from your length units to mm
-    #[arg(long)]
+    #[arg(long, value_parser=f32_is_positive)]
     to_mm:Option<f32>,
 
     /// optional coversion factor from your weight units to kg
-    #[arg(long)]
+    #[arg(long, value_parser=f32_is_positive)]
     to_kg:Option<f32>
 }
 
+/// contains all info for one type of package
 struct Package{
     name:String,
-    cost:f32,
-    dimensions:Vec<u32>
+    shipping_cost:f32,
+    dimensions_mm:Vec<f32>
 }
 
 fn main() {
-
     // parse input values
     let cli = Cli::parse();
     println!("cli: {:?}", cli);
 
     // make a parcel with sorted dimensions
-    let mut parcel: Vec<u32>= vec![cli.x, cli.y, cli.z];
-    parcel.sort_unstable();
+    let mut parcel: Vec<f32>= vec![cli.x, cli.y, cli.z];
+    parcel.sort_unstable_by(|a,b| a.partial_cmp(b).unwrap());
 
     // check for weight conversions
     let mut weight = cli.w;
@@ -54,7 +67,7 @@ fn main() {
 
     // check for length conversions
     match cli.to_mm{
-        Some(to_mm) => { for d in parcel.iter_mut() { *d = ( (*d as f32) * (to_mm as f32)) as u32; } },
+        Some(to_mm) => { for d in parcel.iter_mut() { *d = *d * to_mm; } },
         None => {}
     }
 
@@ -68,24 +81,24 @@ fn main() {
 
     // make a vec of Packages with UNsorted dimensions
     let mut packages = Vec::<Package>::new();
-    packages.push(Package { name: "small".to_string(),  cost: 5.00, dimensions: vec![200, 300, 150] });
-    packages.push(Package { name: "medium".to_string(), cost: 7.50, dimensions: vec![300 ,400, 200] });
-    packages.push(Package { name: "large".to_string(),  cost: 8.50, dimensions: vec![400, 600, 250] });
+    packages.push(Package { name: "small".to_string(),  shipping_cost: 5.00, dimensions_mm: vec![200.0, 300.0, 150.0] });
+    packages.push(Package { name: "medium".to_string(), shipping_cost: 7.50, dimensions_mm: vec![300.0, 400.0, 200.0] });
+    packages.push(Package { name: "large".to_string(),  shipping_cost: 8.50, dimensions_mm: vec![400.0, 600.0, 250.0] });
 
     // try to find the smallest package that fits
     for mut p in packages {
         // borrow checker says we need to sort package dimensions here(why?)
-        p.dimensions.sort_unstable();   
+        p.dimensions_mm.sort_unstable_by(|a,b| a.partial_cmp(b).unwrap());  
         
         // make an iterator that acts on both parcel and Package dimensions
-        let mut dims = zip(parcel.clone(), p.dimensions);
+        let mut dims = zip(parcel.clone(), p.dimensions_mm);
         
         // check that the parcel fits into the Package in every dimension
         let it_fits = dims.all(|d| d.0<=d.1);
 
         // exit if we found a working solution, else try next Package
         if it_fits {
-            println!("this parcel can ship in a {} container for ${:.2}", p.name, p.cost); 
+            println!("this parcel can ship in a {} container for ${:.2}", p.name, p.shipping_cost); 
             return; 
         }
     }
